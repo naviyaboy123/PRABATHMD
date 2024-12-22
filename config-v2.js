cedb9433e88ba505350cbac4d7045e0d7ab6f852
naviyaboy123 <naviyab979@gmail.com>
2024-12-22 07:54:05 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "94753356130"
+const SESSION_ID = "PRABATH-MD~jG41hSzY#OzV6J2FgpF1k8h0DRqYd3XQQGnvtfiDYczflZqYWTNc"
 
 
 module.exports = {
